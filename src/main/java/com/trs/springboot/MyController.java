393f35848ccfabbd31c89f4a1d27d980b4ec9bc2
Ewind2017 <32627007+Ewind2017@users.noreply.github.com>
2017-10-13 13:09:42 +0800

    Update MyController.java

--- src/main/java/com/trs/springboot/MyController.java
+++ src/main/java/com/trs/springboot/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Title:
+ * Title:toPull2
  * Description:
  * Copyright: 2017 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
  * Company:北京拓尔思信息技术股份有限公司(TRS)
